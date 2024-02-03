a01d5cef9ad0e9fb4608eb7498185dbcf8d717d3
lne <bluefoxtumbleweed@outlook.com>
2024-02-03 11:53:09 +0800

    none

--- projects/temporal.py
+++ projects/temporal.py
@@ -1,10 +1,7 @@
-import networkx as nx
+import datetime
 import argparse
 import pandas as pd
-import itertools
-import operator
-import numpy as np
-from collections import defaultdict
+import networkx as nx
 from rich.console import Console
 
 parser = argparse.ArgumentParser()
@@ -12,14 +9,13 @@ parser.add_argument("dataset")
 args = parser.parse_args()
 console = Console()
 
-df = pd.read_csv(args.dataset,sep=" ",names = ["timestamp", "source", "target"],dtype=int)    
-G = nx.from_pandas_edgelist(df,edge_attr=True)
-console.log(f"Load {G} in [0,{t_max})")
-G.graph["lifespan"] = max(df["timestamp"])+1
+console.rule("load graph from file")
+df = pd.read_csv(args.dataset, sep=" ", names=["source", "target", "timestamp"], dtype=int)
+df["timestamp"] = pd.to_datetime(df["timestamp"],unit="s").dt.date
+df["timestamp"] = df.groupby("timestamp").ngroup()
+console.log(df)
+G = nx.from_pandas_edgelist(df)
+G.graph["lifespan"] = len(df["timestamp"].unique())
+console.log(f"Load {G} in [0,{G.graph['lifespan']})")
 
-def construct_phc(G:nx.Graph):
-    nx.core_number(G)
-    core_neighbor = np.array((G.graph["lifespan"], G.graph["lifespan"]), dtype=int)
-    for ts in range(G.graph["lifespan"]):
-        for t in reversed(range(ts+1,G.graph["lifespan"])):
-            DelEdges
+
