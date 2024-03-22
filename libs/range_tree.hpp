8ab87f94226eddb89f6bf9c2f306f9121b514036
lne <bluefoxtumbleweed@outlook.com>
2024-03-22 14:50:00 +0800

    null

--- libs/range_tree.hpp
+++ libs/range_tree.hpp
@@ -1,95 +1,105 @@
-#include <vector>
 #include <algorithm>
+#include <iostream>
+#include <memory>
 #include <ranges>
-#include <map>
-#include "../libs/rich.hpp"
+#include <stdexcept>
+#include <tuple>
+#include <vector>
 
-template <typename T>
-struct tree_view{
-  using container = std::span<T>; 
-  using element_type = typename container::element_type;
-  using value_type = typename container::value_type;
-  using size_type = typename container::size_type;
-  using difference_type = typename container::difference_type;
-  using reference = typename container::reference;
-  using self_type = tree_view<T>;
-  [[nodiscard]] reference root() const noexcept { return c[c.size()/2];}
-  [[nodiscard]] constexpr self_type lower_child() const noexcept { return c.subspan(0,c.size()/2);}
-  [[nodiscard]] constexpr self_type upper_child() const noexcept { return c.subspan(c.size()/2+1);}
-  [[nodiscard]] constexpr bool empty() const noexcept { return c.empty();} 
-  [[nodiscard]] constexpr bool has_lower_child() const noexcept { return c.size()>=2;}
-  [[nodiscard]] constexpr bool has_upper_child() const noexcept { return c.size()>=3;}
-  [[nodiscard]] constexpr size_type size() const noexcept { return c.size();}
-  tree_view(container c):c(c){}
-  
-  container c;
-};
-
-template <typename TPoint, size_t Dimension = std::tuple_size_v<TPoint>>
-class range_tree_node{
+template <typename TPoint, typename TValue, size_t Dimension = std::tuple_size_v<TPoint>>
+class range_tree {
 public:
   using point_type = TPoint;
-  using key_type = std::tuple_element_t<Dimension-1,point_type>;
-  using node_type = range_tree_node<TPoint,Dimension-1>;
-  key_type key;
-  range_tree_node() = default;
-  range_tree_node(std::span<point_type> points):nodes(2*points.size()-1){
-    rich::console.println("init node of dim {} with points {}", Dimension, points);
-    std::ranges::sort(points,{},[](const auto&x){return std::get<Dimension-1>(x);});
-    key = std::get<Dimension-1>(points[points.size()/2]);
-    build_tree(tree_view<node_type>{std::span<node_type>{nodes}},points);
-  }
-  void build_tree(tree_view<node_type> tree, std::span<point_type> points){
-    rich::console.println("size of tree {} and of points {}", tree.size(), points.size());
-    if(points.size()==0) return;
-    auto mid = (points.size()-1)/2;
-    if(!tree.empty())
-      tree.root() = node_type{points};
-    if(tree.has_lower_child())
-      build_tree(tree.lower_child(), points.subspan(0,mid+1));
-    if(tree.has_upper_child())
-      build_tree(tree.upper_child(), points.subspan(mid+1));
-  }
-  auto query(const point_type& lower, const point_type upper){
-    auto tree = tree_view<node_type>{std::span<node_type>{nodes}};
-    while(tree.root().key)
-  }
-  const auto& data()const { return nodes;}
-private:
-  std::vector<node_type> nodes;
-};
-
-template <typename TPoint>
-class range_tree_node<TPoint,0>{
-public:
-  range_tree_node() = default;
-  using point_type = TPoint;
-  using key_type = std::tuple_element_t<0,point_type>;
-  range_tree_node(std::span<point_type> points){
-    key = std::get<0>(points[(points.size()-1)/2]);
-  }
-  key_type key;
-};
-
-template <typename TPoint, typename TVal>
-class range_tree{
-public:
-  using value_type = TVal;
-  using point_type = TPoint;
+  using value_type = TValue;
   using size_type = size_t;
-  template <std::ranges::input_range R>
-  explicit range_tree(R&&rng){
-    std::ranges::copy(rng|std::views::values,std::back_inserter(values));
-    std::vector<point_type> points(values.size());
-    std::ranges::copy(rng|std::views::keys,points.begin());
-    rich::console.println("the points are {}",points);
-    root = range_tree_node<point_type>{points};
+  using key_type = std::tuple_element_t<Dimension - 1, TPoint>;
+
+  range_tree(std::span<point_type> points) {
+    if (points.empty()) throw std::logic_error("construct range tree with no point");
+    std::ranges::sort(points, {}, [](const point_type &x) { return std::get<Dimension - 1>(x); });
+    auto mid = (points.size()-1) / 2;
+    // std::cout<<mid<<" ";
+    _key = std::get<Dimension - 1>(points[mid]);
+    if(points.size()!=1){
+      auto lower_span = points.subspan(0, (points.size() + 1) / 2);
+      if (!lower_span.empty())
+        lower_child = std::make_shared<range_tree<TPoint,TValue,Dimension>>(lower_span);
+      auto upper_span = points.subspan((points.size() + 1) / 2);
+      if (!upper_span.empty())
+        upper_child = std::make_shared<range_tree<TPoint,TValue,Dimension>>(upper_span);
+    }
+    next_dim = std::make_shared<range_tree<TPoint,TValue,Dimension-1>>(points);
   }
-  const auto data() const { return root.data();}
-  auto query(const point_type &lower, const point_type &upper ){
-    root.query(lower,upper);
+  template <std::output_iterator<key_type> Iter>
+  void query(const point_type &lower, const point_type &upper, Iter result) {
+    auto lower_key = std::get<Dimension - 1>(lower);
+    auto upper_key = std::get<Dimension - 1>(upper);
+    if(is_leaf()){
+      if (lower_key<=_key &&_key<=upper_key)
+        next_dim->query(lower,upper,result);
+    } else if (upper_key <= _key) {
+      if(lower_child)
+        lower_child->query(lower, upper, result);
+    } else if (lower_key > _key) {
+      if(upper_child)
+        upper_child->query(lower, upper,result);
+    } else {
+      auto lower_cur = lower_child;
+      while(lower_cur){
+        if (!lower_cur->lower_child&&!lower_cur->upper_child){
+          if(lower_key<=lower_cur->_key) lower_cur->next_dim->query(lower,upper,result);
+          break;          
+        }
+        else if(lower_key<=lower_cur->_key){
+          if(lower_cur->upper_child)
+            lower_cur->upper_child->next_dim->query(lower,upper,result);
+          lower_cur = lower_cur->lower_child;
+        } else lower_cur = lower_cur->upper_child;
+      }
+      auto upper_cur = upper_child;
+      while(upper_cur){
+        if (!upper_cur->lower_child&&!upper_cur->upper_child){
+          if(upper_key>=upper_cur->_key) upper_cur->next_dim->query(lower,upper,result);
+          break;          
+        }
+        else if(upper_key>upper_cur->_key){
+          if(upper_cur->lower_child)
+            upper_cur->lower_child->next_dim->query(lower,upper,result);
+          upper_cur = upper_cur->upper_child;
+        } else upper_cur = upper_cur->lower_child;
+      }
+    }
   }
+
 private:
-  std::vector<value_type> values;
-  range_tree_node<point_type> root;
+  bool is_leaf(){return !lower_child&&!upper_child;}
+  std::shared_ptr<range_tree<TPoint, TValue, Dimension>> lower_child;
+  std::shared_ptr<range_tree<TPoint, TValue, Dimension>> upper_child;
+  std::shared_ptr<range_tree<TPoint, TValue, Dimension - 1>> next_dim;
+  key_type _key;
+};
+
+template <typename TPoint, typename TValue>
+class range_tree<TPoint,TValue ,1> {
+public:
+  using point_type = TPoint;
+  using value_type = TValue;
+  using size_type = size_t;
+  using key_type = std::tuple_element_t<0, TPoint>;
+
+  range_tree(std::span<point_type> points) {
+    if (points.empty())
+      throw std::logic_error("construct range tree with no point");
+    std::ranges::sort(points, {}, [](const point_type &x) { return std::get<0>(x); });
+    std::ranges::copy(points | std::views::elements<0>, std::back_inserter(_data));
+  }
+  template <std::output_iterator<key_type> Iter>
+  void query(const point_type &lower, const point_type &upper, Iter result) {
+    auto lower_iter = std::ranges::lower_bound(_data, std::get<0>(lower));
+    auto upper_iter = std::ranges::upper_bound(_data, std::get<0>(upper));
+    std::copy(lower_iter, upper_iter, result);
+  }
+
+private:
+  std::vector<key_type> _data;
 };
--- playgrounds/main.cpp
+++ playgrounds/main.cpp
@@ -6,10 +6,11 @@
 #include <tuple>
 
 int main(){
+  using index_type = size_t;
   using value_t = size_t;
   using stamp_t = size_t;
   auto ifs = std::ifstream{"data/graph.txt"};
-  auto [num_vert,edges] = rich::io::load<std::pair<size_t,std::vector<std::tuple<value_t,value_t,stamp_t>>>>(ifs).value();
+  auto [num_vert,num_time, edges] = rich::io::load<std::tuple<size_t,size_t,std::vector<std::tuple<value_t,value_t,stamp_t>>>>(ifs).value();
   auto index = temporal_index<value_t,stamp_t>{num_vert};
   // std::ranges::sort(edges,{},[](const auto&edge){
   //   auto [u,v,t] = edge;
@@ -18,11 +19,38 @@ int main(){
     auto [u,v,t] = x;
     index.update(u,v,t,t);
   });});
+  // std::ranges::sort(index.index,{},[](const auto&x){return std::get<2>(x);});
   rich::console.println("generate temporal spanning forest of size {}",index.size());
-  while(true){
-    auto [ts,te] = rich::console.read<std::pair<stamp_t,stamp_t>>("input an interval");
-    index.query(ts,te);
-  }
+  auto query_ifs = std::ifstream{"data/query.txt"};
+  auto output_ofs = std::ofstream{"data/output.txt"};
+  auto queries = rich::io::load<std::vector<std::pair<size_t,size_t>>>(query_ifs).value();
+  //! i build it here
+  index.build();
+  rich::console.bench([&]{
+    for (auto [ts,te]: queries){
+      auto etf = index.query(ts,te);
+      auto is_visited = std::vector<bool>(num_vert,false);
+      output_ofs << std::format("The spanned connected components in [{}, {}] are:\n",ts,te);
+      for(auto i:std::views::iota(0U,num_vert))
+      {
+        if(!is_visited[i])
+        {
+          auto temp = std::vector<index_type>{};
+          auto v = i;
+          do{
+            v = etf.next(v);
+            temp.push_back(v);
+            is_visited[v] = true;
+          } while(v!=i);
+          std::ranges::sort(temp);
+          output_ofs << "{ ";
+          for(auto x:temp) output_ofs<<x<<" ";
+          output_ofs << "}\n";
+        }
+      }
+      output_ofs<<"\n";
+    }
+  });
 }
 
 
